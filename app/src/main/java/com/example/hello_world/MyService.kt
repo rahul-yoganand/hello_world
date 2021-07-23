885a6c8c1754411e11db57a9f16795da4ae50ed7
rahulyoganand <pry10@iitbbs.ac.in>
2021-07-23 11:43:14 +0530

    Service

---
+++ app/src/main/java/com/example/hello_world/MyService.kt
@@ -0,0 +1,26 @@
+package com.example.hello_world
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+
+class MyService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Toast.makeText(this,"Service Created",Toast.LENGTH_SHORT).show()
+    }
+
+//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+//        return super.onStartCommand(intent, flags, startId)
+//    }
+    //override fun onBind(intent: Intent): IBinder {
+   // }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"Service Created",Toast.LENGTH_SHORT).show()
+    }
+}
